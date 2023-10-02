e36d6bd0ecd592e2263a7bd4009c68cc752a344c
Kim Wool <kwool0410@gmail.com>
2023-10-02 21:33:02 +0900

    프레임 확장 및 보드판(Tetris) 위치 조정

--- tetris/src/kr/ac/jbnu/se/tetris/Tetris.java
+++ tetris/src/kr/ac/jbnu/se/tetris/Tetris.java
@@ -1,29 +1,49 @@
 package kr.ac.jbnu.se.tetris;
 
-import java.awt.BorderLayout;
+import java.awt.*;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.*;
 
 // 메인 함수 클래스
 public class Tetris extends JFrame {
-
-	final int gameWidth = 200;
-	final int gameHeight = 400;
+	final int executionWidth = 800;
+	final int executionHeight = 800;
 
 	JLabel statusbar;
 
 	public Tetris() {
-
 		statusbar = new JLabel(" 0");
 		add(statusbar, BorderLayout.SOUTH);
+
 		Board board = new Board(this);
-		add(board);
+		board.setPreferredSize(new Dimension(200, 400)); // 게임 보드의 크기 설정
+
+		JPanel containerPanel = new JPanel(new BorderLayout());
+		containerPanel.setPreferredSize(new Dimension(executionWidth, executionHeight));
+		containerPanel.add(board, BorderLayout.CENTER);
+
+		// 나머지 여백 패널 생성
+		JPanel fillerPanelN = new JPanel();
+		fillerPanelN.setPreferredSize(new Dimension(executionWidth, (executionHeight - 400) / 2));
+		JPanel fillerPanelE = new JPanel();
+		fillerPanelE.setPreferredSize(new Dimension((executionWidth - 200) / 2, 400));
+		JPanel fillerPanelW = new JPanel();
+		fillerPanelW.setPreferredSize(new Dimension((executionWidth - 200) / 2, 400));
+		JPanel fillerPanelS = new JPanel();
+		fillerPanelS.setPreferredSize(new Dimension(executionWidth, (executionHeight - 400) / 2));
+
+		containerPanel.add(fillerPanelN, BorderLayout.NORTH);
+		containerPanel.add(fillerPanelE, BorderLayout.EAST);
+		containerPanel.add(fillerPanelW, BorderLayout.WEST);
+		containerPanel.add(fillerPanelS, BorderLayout.SOUTH);
+
+		add(containerPanel);
 		board.start();
 
-		setSize(gameWidth, gameHeight);
+		pack();
 		setTitle("Tetris");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
 	}
 
 	public JLabel getStatusBar() {
@@ -32,7 +52,6 @@ public class Tetris extends JFrame {
 
 	public static void main(String[] args) {
 		Tetris game = new Tetris();
-		game.setLocationRelativeTo(null);
 		game.setVisible(true);
 	}
 }

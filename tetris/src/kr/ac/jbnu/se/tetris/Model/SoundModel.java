798e8ecd128c04e49d3d7d7637df8a076cbced17
HanJunSeo <hjs123@jbnu.ac.kr>
2023-11-21 11:12:18 +0900

    수정 된 코드

--- tetris/src/kr/ac/jbnu/se/tetris/Model/SoundModel.java
+++ tetris/src/kr/ac/jbnu/se/tetris/Model/SoundModel.java
@@ -4,11 +4,9 @@ import java.io.File;
 import javax.sound.sampled.*;
 
 public class SoundModel {
-    /***************************************************************/
     // 객체 초기화
     AudioInputStream ais;
     Clip clip;
-    /***************************************************************/
 
     // 볼륨 설정 메소드
     public void setVolume(float volume) {
@@ -16,7 +14,6 @@ public class SoundModel {
             FloatControl volumeControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
             float min = volumeControl.getMinimum();
             float max = volumeControl.getMaximum();
-            // volume 파라미터는 0.0 (최소)에서 1.0 (최대) 사이의 값이어야 합니다.
             float value = min + (max - min) * volume;
             volumeControl.setValue(value);
         }
@@ -26,7 +23,6 @@ public class SoundModel {
     public void introBgmPlay() {
         try {
             ais = AudioSystem.getAudioInputStream(new File("tetris/src/sound/bgm.wav"));
-            //"src/sound/bgm.wav"
             clip = AudioSystem.getClip();
             clip.open(ais);
             clip.start();
@@ -34,12 +30,10 @@ public class SoundModel {
             System.out.println(e.getMessage());
         }
     }
-    /***************************************************************/
     // intro bgm stop
     public void intoBgmStop() {
         clip.stop();
     }
-    /***************************************************************/
     // playBgm
     public void playBgm() {
         try {
@@ -51,44 +45,7 @@ public class SoundModel {
             System.out.println(e.getMessage());
         }
     }
-    /***************************************************************/
 
-    // victory
-    public void victoryPlay() {
-        try {
-            AudioInputStream ais = AudioSystem.getAudioInputStream(new File("tetris/sound/victory.wav"));
-            Clip clip = AudioSystem.getClip();
-            clip.open(ais);
-            clip.start();
-        } catch(Exception e) {
-            System.out.println(e.getMessage());
-        }
-    }
-    /***************************************************************/
-    // lose
-    public void losePlay() {
-        try {
-            AudioInputStream ais = AudioSystem.getAudioInputStream(new File("tetris/sound/lose.wav"));
-            Clip clip = AudioSystem.getClip();
-            clip.open(ais);
-            clip.start();
-        } catch(Exception e) {
-            System.out.println(e.getMessage());
-        }
-    }
-    /***************************************************************/
-    // menu click
-    public void menuClickPlay() {
-        try {
-            AudioInputStream ais = AudioSystem.getAudioInputStream(new File("tetris/sound/menu_click.wav"));
-            Clip clip = AudioSystem.getClip();
-            clip.open(ais);
-            clip.start();
-        } catch(Exception e) {
-            System.out.println(e.getMessage());
-        }
-    }
-    /***************************************************************/
     // move block
     public void dropBlockPlay() {
         try {
